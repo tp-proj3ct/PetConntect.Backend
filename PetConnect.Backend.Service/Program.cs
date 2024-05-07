8bec0e37205c6a48401181a4dd7c0437fe378542
Unusualy1 <syzuki45@gmail.com>
2024-05-07 18:41:29 +0700

    [Service] Created template program.cs

--- PetConnect.Backend.Service/Program.cs
+++ PetConnect.Backend.Service/Program.cs
@@ -1,28 +1,86 @@
-using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-   .AddNegotiate();
-
-builder.Services.AddAuthorization(options =>
+namespace MNX.MonitoringCenter.Management;
+public static class Program
 {
-    // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
-});
+    public static async Task Main(string[] args)
+    {
+        var builder = ConfigureApp(args);
+        await RunApp(builder);
+    }
 
-var app = builder.Build();
+    private static WebApplicationBuilder ConfigureApp(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-// Configure the HTTP request pipeline.
+        var services = builder.Services;
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policy =>
+            {
+                policy.AllowAnyOrigin()
+                      .AllowAnyHeader()
+                      .AllowAnyMethod();
+            });
+        });
 
-app.UseHttpsRedirection();
+        services.AddHealthChecks();
+        services.AddSwaggerGen(options =>
+        {
+            //var basePath = AppContext.BaseDirectory;
+            //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //var xmlPath = Path.Combine(basePath, xmlFile);
+            //options.IncludeXmlComments(xmlPath);
+        });
 
-app.UseAuthorization();
+        ConfigureDI(services, builder.Configuration);
 
-app.MapControllers();
+        return builder;
+    }
+    private static void ConfigureDI(IServiceCollection services, ConfigurationManager configuration)
+    {
+        // DB Context
+        var connectionStrings = configuration.GetConnectionString("DefaultConnection");
+        //services.AddDbContext<Context>(options =>
+        //{
+        //    options.UseNpgsql(connectionStrings);
+        //});
+        // MediatR
+        //services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(AddPostCommand).Assembly));
+        // Dependencies
+        
+    }
 
-app.Run();
+    private static async Task RunApp(WebApplicationBuilder builder)
+    {
+        var app = builder.Build();
+        var appName = builder.Configuration["ServiceName"]?? throw new ArgumentNullException("ServiceName", "?? ??????? ???????? ???????");
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
+        }
+
+        app.UseRouting();
+        app.UseCors();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+        app.MapHealthChecks("/health");
+        app.MapGet(string.Empty, async ctx => await ctx.Response.WriteAsync(appName));
+
+        await app.RunAsync();
+    }
+}

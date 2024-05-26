1296c9bcd7ca26132fd8badbfbe858743148eea1
Unusualy1 <syzuki45@gmail.com>
2024-05-26 20:14:38 +0700

    [ETC] Added packages

---
+++ PetConnect.Backend.Packages/UseCases/ResultStatus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetConnect.Backend.Packages.UseCases;
+
+/// <summary>
+/// Статусы выполнения команд и запросов
+/// </summary>
+public enum ResultStatus
+{
+    /// <summary>
+    /// Статус не установлен
+    /// </summary>
+    Unknown = 0,
+    /// <summary>
+    /// Выполнено успешно
+    /// </summary>
+    Ok = 200,
+    /// <summary>
+    /// Выполнено успешно и создан новый ресурс
+    /// </summary>
+    Created = 201,
+    /// <summary>
+    /// Выполнено успешно. Возвращать результат не требуется
+    /// </summary>
+    NoContent = 204,
+    /// <summary>
+    /// Некорректные параметры команды/запроса
+    /// </summary>
+    Invalid = 400,
+    /// <summary>
+    /// Действие недоступно
+    /// </summary>
+    Forbidden = 403,
+    /// <summary>
+    /// Объект для создания конфликтует с уже существующими объектами
+    /// </summary>
+    Conflict = 409,
+    /// <summary>
+    /// Ошибка выполнения
+    /// </summary>
+    Error = 422,
+}
